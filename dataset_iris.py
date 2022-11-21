aa9a2aa85a6ee67419a8d226f41ee552d59a0c2e
EBLANCO17 <emersonblanco2015@gmail.com>
2022-11-21 13:25:53 -0500

    FeedForward dataset-iris

---
+++ dataset_iris.py
@@ -0,0 +1,270 @@
+datos=[
+#['Longitud sepalo', 'ancho sepalo', 'Longitud petalo', 'Ancho petalo', 'Tipo'],
+[5.1,3.5,1.4,0.2,'Iris-setosa'],
+[4.9,3.0,1.4,0.2,'Iris-setosa'],
+[4.7,3.2,1.3,0.2,'Iris-setosa'],
+[4.6,3.1,1.5,0.2,'Iris-setosa'],
+[5.0,3.6,1.4,0.2,'Iris-setosa'],
+[5.4,3.9,1.7,0.4,'Iris-setosa'],
+[4.6,3.4,1.4,0.3,'Iris-setosa'],
+[5.0,3.4,1.5,0.2,'Iris-setosa'],
+[4.4,2.9,1.4,0.2,'Iris-setosa'],
+[4.9,3.1,1.5,0.1,'Iris-setosa'],
+[5.4,3.7,1.5,0.2,'Iris-setosa'],
+[4.8,3.4,1.6,0.2,'Iris-setosa'],
+[4.8,3.0,1.4,0.1,'Iris-setosa'],
+[4.3,3.0,1.1,0.1,'Iris-setosa'],
+[5.8,4.0,1.2,0.2,'Iris-setosa'],
+[5.7,4.4,1.5,0.4,'Iris-setosa'],
+[5.4,3.9,1.3,0.4,'Iris-setosa'],
+[5.1,3.5,1.4,0.3,'Iris-setosa'],
+[5.7,3.8,1.7,0.3,'Iris-setosa'],
+[5.1,3.8,1.5,0.3,'Iris-setosa'],
+[5.4,3.4,1.7,0.2,'Iris-setosa'],
+[5.1,3.7,1.5,0.4,'Iris-setosa'],
+[4.6,3.6,1.0,0.2,'Iris-setosa'],
+[5.1,3.3,1.7,0.5,'Iris-setosa'],
+[4.8,3.4,1.9,0.2,'Iris-setosa'],
+[5.0,3.0,1.6,0.2,'Iris-setosa'],
+[5.0,3.4,1.6,0.4,'Iris-setosa'],
+[5.2,3.5,1.5,0.2,'Iris-setosa'],
+[5.2,3.4,1.4,0.2,'Iris-setosa'],
+[4.7,3.2,1.6,0.2,'Iris-setosa'],
+[4.8,3.1,1.6,0.2,'Iris-setosa'],
+[5.4,3.4,1.5,0.4,'Iris-setosa'],
+[5.2,4.1,1.5,0.1,'Iris-setosa'],
+[5.5,4.2,1.4,0.2,'Iris-setosa'],
+[4.9,3.1,1.5,0.1,'Iris-setosa'],
+[5.0,3.2,1.2,0.2,'Iris-setosa'],
+[5.5,3.5,1.3,0.2,'Iris-setosa'],
+[4.9,3.1,1.5,0.1,'Iris-setosa'],
+[4.4,3.0,1.3,0.2,'Iris-setosa'],
+[5.1,3.4,1.5,0.2,'Iris-setosa'],
+[5.0,3.5,1.3,0.3,'Iris-setosa'],
+[4.5,2.3,1.3,0.3,'Iris-setosa'],
+[4.4,3.2,1.3,0.2,'Iris-setosa'],
+[5.0,3.5,1.6,0.6,'Iris-setosa'],
+[5.1,3.8,1.9,0.4,'Iris-setosa'],
+[4.8,3.0,1.4,0.3,'Iris-setosa'],
+[5.1,3.8,1.6,0.2,'Iris-setosa'],
+[4.6,3.2,1.4,0.2,'Iris-setosa'],
+[5.3,3.7,1.5,0.2,'Iris-setosa'],
+[5.0,3.3,1.4,0.2,'Iris-setosa'],
+[7.0,3.2,4.7,1.4,'Iris-versicolor'],
+[6.4,3.2,4.5,1.5,'Iris-versicolor'],
+[6.9,3.1,4.9,1.5,'Iris-versicolor'],
+[5.5,2.3,4.0,1.3,'Iris-versicolor'],
+[6.5,2.8,4.6,1.5,'Iris-versicolor'],
+[5.7,2.8,4.5,1.3,'Iris-versicolor'],
+[6.3,3.3,4.7,1.6,'Iris-versicolor'],
+[4.9,2.4,3.3,1.0,'Iris-versicolor'],
+[6.6,2.9,4.6,1.3,'Iris-versicolor'],
+[5.2,2.7,3.9,1.4,'Iris-versicolor'],
+[5.0,2.0,3.5,1.0,'Iris-versicolor'],
+[5.9,3.0,4.2,1.5,'Iris-versicolor'],
+[6.0,2.2,4.0,1.0,'Iris-versicolor'],
+[6.1,2.9,4.7,1.4,'Iris-versicolor'],
+[5.6,2.9,3.6,1.3,'Iris-versicolor'],
+[6.7,3.1,4.4,1.4,'Iris-versicolor'],
+[5.6,3.0,4.5,1.5,'Iris-versicolor'],
+[5.8,2.7,4.1,1.0,'Iris-versicolor'],
+[6.2,2.2,4.5,1.5,'Iris-versicolor'],
+[5.6,2.5,3.9,1.1,'Iris-versicolor'],
+[5.9,3.2,4.8,1.8,'Iris-versicolor'],
+[6.1,2.8,4.0,1.3,'Iris-versicolor'],
+[6.3,2.5,4.9,1.5,'Iris-versicolor'],
+[6.1,2.8,4.7,1.2,'Iris-versicolor'],
+[6.4,2.9,4.3,1.3,'Iris-versicolor'],
+[6.6,3.0,4.4,1.4,'Iris-versicolor'],
+[6.8,2.8,4.8,1.4,'Iris-versicolor'],
+[6.7,3.0,5.0,1.7,'Iris-versicolor'],
+[6.0,2.9,4.5,1.5,'Iris-versicolor'],
+[5.7,2.6,3.5,1.0,'Iris-versicolor'],
+[5.5,2.4,3.8,1.1,'Iris-versicolor'],
+[5.5,2.4,3.7,1.0,'Iris-versicolor'],
+[5.8,2.7,3.9,1.2,'Iris-versicolor'],
+[6.0,2.7,5.1,1.6,'Iris-versicolor'],
+[5.4,3.0,4.5,1.5,'Iris-versicolor'],
+[6.0,3.4,4.5,1.6,'Iris-versicolor'],
+[6.7,3.1,4.7,1.5,'Iris-versicolor'],
+[6.3,2.3,4.4,1.3,'Iris-versicolor'],
+[5.6,3.0,4.1,1.3,'Iris-versicolor'],
+[5.5,2.5,4.0,1.3,'Iris-versicolor'],
+[5.5,2.6,4.4,1.2,'Iris-versicolor'],
+[6.1,3.0,4.6,1.4,'Iris-versicolor'],
+[5.8,2.6,4.0,1.2,'Iris-versicolor'],
+[5.0,2.3,3.3,1.0,'Iris-versicolor'],
+[5.6,2.7,4.2,1.3,'Iris-versicolor'],
+[5.7,3.0,4.2,1.2,'Iris-versicolor'],
+[5.7,2.9,4.2,1.3,'Iris-versicolor'],
+[6.2,2.9,4.3,1.3,'Iris-versicolor'],
+[5.1,2.5,3.0,1.1,'Iris-versicolor'],
+[5.7,2.8,4.1,1.3,'Iris-versicolor'],
+[6.3,3.3,6.0,2.5,'Iris-virginica'],
+[5.8,2.7,5.1,1.9,'Iris-virginica'],
+[7.1,3.0,5.9,2.1,'Iris-virginica'],
+[6.3,2.9,5.6,1.8,'Iris-virginica'],
+[6.5,3.0,5.8,2.2,'Iris-virginica'],
+[7.6,3.0,6.6,2.1,'Iris-virginica'],
+[4.9,2.5,4.5,1.7,'Iris-virginica'],
+[7.3,2.9,6.3,1.8,'Iris-virginica'],
+[6.7,2.5,5.8,1.8,'Iris-virginica'],
+[7.2,3.6,6.1,2.5,'Iris-virginica'],
+[6.5,3.2,5.1,2.0,'Iris-virginica'],
+[6.4,2.7,5.3,1.9,'Iris-virginica'],
+[6.8,3.0,5.5,2.1,'Iris-virginica'],
+[5.7,2.5,5.0,2.0,'Iris-virginica'],
+[5.8,2.8,5.1,2.4,'Iris-virginica'],
+[6.4,3.2,5.3,2.3,'Iris-virginica'],
+[6.5,3.0,5.5,1.8,'Iris-virginica'],
+[7.7,3.8,6.7,2.2,'Iris-virginica'],
+[7.7,2.6,6.9,2.3,'Iris-virginica'],
+[6.0,2.2,5.0,1.5,'Iris-virginica'],
+[6.9,3.2,5.7,2.3,'Iris-virginica'],
+[5.6,2.8,4.9,2.0,'Iris-virginica'],
+[7.7,2.8,6.7,2.0,'Iris-virginica'],
+[6.3,2.7,4.9,1.8,'Iris-virginica'],
+[6.7,3.3,5.7,2.1,'Iris-virginica'],
+[7.2,3.2,6.0,1.8,'Iris-virginica'],
+[6.2,2.8,4.8,1.8,'Iris-virginica'],
+[6.1,3.0,4.9,1.8,'Iris-virginica'],
+[6.4,2.8,5.6,2.1,'Iris-virginica'],
+[7.2,3.0,5.8,1.6,'Iris-virginica'],
+[7.4,2.8,6.1,1.9,'Iris-virginica'],
+[7.9,3.8,6.4,2.0,'Iris-virginica'],
+[6.4,2.8,5.6,2.2,'Iris-virginica'],
+[6.3,2.8,5.1,1.5,'Iris-virginica'],
+[6.1,2.6,5.6,1.4,'Iris-virginica'],
+[7.7,3.0,6.1,2.3,'Iris-virginica'],
+[6.3,3.4,5.6,2.4,'Iris-virginica'],
+[6.4,3.1,5.5,1.8,'Iris-virginica'],
+[6.0,3.0,4.8,1.8,'Iris-virginica'],
+[6.9,3.1,5.4,2.1,'Iris-virginica'],
+[6.7,3.1,5.6,2.4,'Iris-virginica'],
+[6.9,3.1,5.1,2.3,'Iris-virginica'],
+[5.8,2.7,5.1,1.9,'Iris-virginica'],
+[6.8,3.2,5.9,2.3,'Iris-virginica'],
+[6.7,3.3,5.7,2.5,'Iris-virginica'],
+[6.7,3.0,5.2,2.3,'Iris-virginica'],
+[6.3,2.5,5.0,1.9,'Iris-virginica'],
+[6.5,3.0,5.2,2.0,'Iris-virginica'],
+[6.2,3.4,5.4,2.3,'Iris-virginica'],
+[5.9,3.0,5.1,1.8,'Iris-virginica']
+]
+
+from math import exp
+from random import seed
+from random import random
+
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
+
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+	activation = weights[-1]
+	for i in range(len(weights)-1):
+		activation += weights[i] * inputs[i]
+	return activation
+
+# Transfer neuron activation
+def transfer(activation):
+	return 1.0 / (1.0 + exp(-activation))
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = []
+		for neuron in layer:
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = transfer(activation)
+			new_inputs.append(neuron['output'])
+		inputs = new_inputs
+	return inputs
+
+# Calculate the derivative of an neuron output
+def transfer_derivative(output):
+	return output * (1.0 - output)
+
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = list()
+		if i != len(network)-1:
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in network[i + 1]:
+					error += (neuron['weights'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(neuron['output'] - expected[j])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+# Update network weights with error
+def update_weights(network, row, l_rate):
+	for i in range(len(network)):
+		inputs = row[:-1]
+		if i != 0:
+			inputs = [neuron['output'] for neuron in network[i - 1]]
+		for neuron in network[i]:
+			for j in range(len(inputs)):
+				neuron['weights'][j] -= l_rate * neuron['delta'] * inputs[j]
+			neuron['weights'][-1] -= l_rate * neuron['delta']
+
+# Train a network for a fixed number of epochs
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+	for epoch in range(n_epoch):
+		sum_error = 0
+		for row in train:
+			outputs = forward_propagate(network, row)
+			expected = [0 for i in range(n_outputs)]
+			expected[row[-1]] = 1
+			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+			backward_propagate_error(network, expected)
+			update_weights(network, row, l_rate)
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+# Test training backprop algorithm
+seed(1)
+dataset = list()
+
+for d in range(len(datos)):
+        if datos[d][4] == 'Iris-setosa':
+            id = 0
+        elif datos[d][4] == 'Iris-versicolor':
+            id = 1
+        elif datos[d][4] == 'Iris-virginica':
+            id = 2
+        dataset.append([datos[d][0],datos[d][1],datos[d][2],datos[d][3],id])
+
+n_inputs = len(dataset[0]) - 1
+n_outputs = len(set([row[-1] for row in dataset]))
+network = initialize_network(n_inputs, 2, n_outputs)
+train_network(network, dataset, 0.5, 20, n_outputs)
+for layer in network:
+	print(layer)
+
+
+# Test making predictions with the network
+ds = [5.0,3.3,1.4,0.2]
+prediction = forward_propagate(network, ds)
+print(prediction)
+prediction = prediction.index(max(prediction))
+print(prediction)
+if prediction == 0:
+    print('Iris-setosa')
+elif prediction == 1:
+    print('Iris-versicolor')
+elif prediction == 2:
+    print('Iris-virginica')
